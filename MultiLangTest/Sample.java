f0d02858365a9d3010513bedeceb422c90a272cb
Aakash <aaakash@academic.rrc.ca>
2025-03-16 02:26:55 -0500

    Add CodeSweep GitHub action.

--- MultiLangTest/Sample.java
+++ MultiLangTest/Sample.java
@@ -3,18 +3,18 @@ import java.util.Scanner;
 public class Sample {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
         // Prompt user for input
         System.out.print("Enter your age: ");
         int age = scanner.nextInt();
-        
+
         // Check age validity
         if (age < 0) {
             System.out.println("Invalid age entered.");
         } else {
             System.out.println("Your age is " + age);
         }
-        
+
         scanner.close();
     }
 }
